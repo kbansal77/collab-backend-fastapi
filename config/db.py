18ea419c90c0604134f5efc51bc6cd510894ba52
Kartik Bansal <kartikbansal2000@gmail.com>
2023-05-12 20:26:33 +0530

    final depoly

--- config/db.py
+++ config/db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 
-username = quote_plus('collab')
-password = quote_plus('newpassword')
+# username = quote_plus('collab')
+# password = quote_plus('newpassword')
 
-conn = MongoClient("mongodb+srv://"+username+":"+password+"@cluster0.qlblf.mongodb.net/collab?retryWrites=true&w=majority")
+conn = MongoClient("mongodb+srv://collab:newpassword@cluster0.qlblf.mongodb.net/collab?retryWrites=true&w=majority")
